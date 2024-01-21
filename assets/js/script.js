73cc965b2a21621c87b1c5f8cbbb143e56d4244d
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-01-21 08:12:25 -0300

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".section");
+
+    function checkVisibility() {
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const isVisible = rect.top <= window.innerHeight / 1;
+
+        // Adiciona ou remove a classe "visible" conforme a visibilidade
+        if (isVisible) {
+          section.classList.add("visible");
+        } else {
+          section.classList.remove("visible");
+        }
+      });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    window.addEventListener("resize", checkVisibility);
+    document.addEventListener("DOMContentLoaded", checkVisibility);
+  });
